1e4c64f8b0f0fb0dc77baaec68262f5fa99abb6a
CihanBosnali <cihanbosnali@gmail.com>
2024-09-15 00:48:50 -0700

    Added lidar interface

--- jetson/main.py
+++ jetson/main.py
@@ -3,7 +3,7 @@ from motors import Drive
 import time
 import cv2
 
-DEBUG = False
+DEBUG = True
 
 
 def main():
@@ -30,10 +30,11 @@ def main():
         d.drive(1, 0)
 
         if DEBUG:
-            print("Latest LIDAR reading", int(time.time() - l_time), "seconds ago")
+            print("Latest LIDAR reading:", l_data)
             print("Latest camera reading", int(time.time() - c_time), "seconds ago")
-            cv2.imshow('a', c_frame)
-            cv2.waitKey(5000)
+            
+            #cv2.imshow('a', c_frame)
+            #cv2.waitKey(5000)
 
 
 if __name__ == "__main__":
--- jetson/sensors/Lidar.py
+++ jetson/sensors/Lidar.py
@@ -1,10 +1,24 @@
 from .Sensor import Sensor
+from rplidar import RPLidar
+import time
+import threading
 
 
 class Lidar(Sensor):
     def __init__(self):
         super()
+        self.lidar = RPLidar('/dev/ttyUSB0')
+        self.scan = 0
+        self.ts = 0
+        print("LIDAR INFO:", self.lidar.get_info())
+        print("LIDAR HEALTH:", self.lidar.get_health())
+        self.t = threading.Thread(target=self.task, name='lidar_task')
+        self.t.start()
+
+    def task(self):
+        for scan in self.lidar.iter_scans():
+            self.scan = scan
+            self.ts = time.time()
 
     def latest(self):
-        print("NOT IMPLEMENTED: Lidar.latest()")
-        return None, 0
+        return self.scan, self.ts
