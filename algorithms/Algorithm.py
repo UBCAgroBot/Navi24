6b5027a39f47598c90bf8a56dbd6e5e7900d1c0b
Arman D <adrismir@gmail.com>
2024-10-23 16:33:43 -0700

    interface for image processing

--- algorithms/Algorithm.py
+++ algorithms/Algorithm.py
@@ -1,12 +1,14 @@
 import numpy as np
 
 class Algorithm:
-    def process_frame(self, frame: np.ndarray) -> float:
+    ## TODO: When I figure out what datatype lidar is update its type here
+    def process_frame(self, frame: np.ndarray, lidar: any) -> float:
         """
         Processes a frame using the algorithm.
 
         Args:
             frame (numpy.ndarray): The current frame to be processed.
+            lidar (any): Current frame of lidar data
 
         Returns:
             (float) the angle AgroBot should move, in the range [-90, 90].
