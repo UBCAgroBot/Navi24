9eb441e226ec86d61d957a2f25c113dcfb968ba7
Arman D <adrismir@gmail.com>
2024-10-23 16:34:51 -0700

    typo lol

--- algorithms/Algorithm.py
+++ algorithms/Algorithm.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 class Algorithm:
-    ## TODO: When I figure out what datatype lidar is update its type here
+    ## TODO: When I figure out what datatype lidar is update it's type here
     def process_frame(self, frame: np.ndarray, lidar: any) -> float:
         """
         Processes a frame using the algorithm.
