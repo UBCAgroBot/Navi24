7517aa9375f7dac90db0a62131b3b7e69d0b9669
Arman D <adrismir@gmail.com>
2024-11-09 11:35:18 -0800

    py module to talk to arduino

--- nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
+++ nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
@@ -3,24 +3,39 @@ import serial
 
 SERIAL_PORT = "/dev/cu.usbmodem1101"
 
-def send_bits(bits:str):
+def send_bits(mode:str, direction:str, speed:str):
     """
     Writes 16bits to the arduino. The 16 bits should follow 
     the protocol in the Navi24 document. Note the bits are 
     passed as a string.
+
+    Example:
+        send_bits("1110011111111111")
     """
-    if not (len(bits) == 16 and all(bit in '01' for bit in bits)):
-        raise ValueError("Input must be a 16-bit string of 0's and 1's.")
+    if len(mode) != 2 and all(bit in '01' for bit in mode):
+        raise ValueError("Mode must be 2 bits and consist of 0 & 1's")
+    if len(direction) != 6 and all (bit in '01' for bit in direction):
+        raise ValueError("Direction must be 6 bits and consist of 0's & 1's")
+    if len(speed) != 8 and all (bit in '01' for bit in speed):
+        raise ValueError("Speed must be 8 bits and consist of 0's & 1's")
+    
+    if mode == '11':
+        raise ValueError("Mode 3,'11' is undefined")
+    # There are 3 invalid direction values, -32, -31, and 31
+    if direction == '100000':
+        raise ValueError("-32 is an invalid direction")
+    if direction == '100001':
+        raise ValueError("-31 is an invalid direction")
+    if direction == '011111':
+        raise ValueError("31 is an invalid direction")
 
     serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None )
     time.sleep(2)
-    out = (bits + '\n').encode('utf-8')
+    message = str(mode + direction + speed)
+    out = (message + '\n').encode('utf-8')
     serial_conn.write(out)
     
     # For testing read the next 2 bits sent back.
     response = serial_conn.read_until()
     print("Raw Arduino response:", response.decode('utf-8'))
     serial_conn.close()
-
-
-send_bits("1110011111111111")
