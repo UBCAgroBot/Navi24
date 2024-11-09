df27527f2289b8423aabde143f196e42c16b763d
Arman D <adrismir@gmail.com>
2024-11-08 22:44:00 -0800

    Update motor_serial to send bits stored in a string.

--- nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
+++ nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
@@ -3,19 +3,24 @@ import serial
 
 SERIAL_PORT = "/dev/cu.usbmodem101"
 
-serial = serial.Serial(
-    port=SERIAL_PORT,
-    baudrate=9600,
-    timeout=1
-)
+def send_bits(bits:str):
+    """
+    Writes 16bits to the arduino. The 16 bits should follow 
+    the protocol in the Navi24 document. Note the bits are 
+    passed as a string.
+    """
+    if not (len(bits) == 16 and all(bit in '01' for bit in bits)):
+        raise ValueError("Input must be a 16-bit string of 0's and 1's.")
 
-message = 0b1111111111111111
-out = message.to_bytes(2, "big", signed=False)
-print("Sending:", out)
-serial.write(out + b"\n")
+    serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None )
+    time.sleep(2)
+    out = (bits + '\n').encode('utf-8')
+    serial_conn.write(out)
+    
+    # For testing read the next 2 bits sent back.
+    response = serial_conn.read_until()
+    print("Raw Arduino response:", response.decode('utf-8'))
+    serial_conn.close()
 
-# Read each byte that Arduino sends back until we receive 2 bytes.
-response = serial.read(2)  # Expecting 2 bytes back
-print("Raw Arduino response:", response)
 
-serial.close()
+send_bits("1110011100111111")
