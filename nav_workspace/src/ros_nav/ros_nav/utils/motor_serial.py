85c2f49803b5a8c6631c56b529871ea1c9e3e032
Arman D <adrismir@gmail.com>
2024-11-24 16:24:53 -0800

    Error resistant serial connector. Dont need mac usb forwarding, it does not work anyway

--- nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
+++ nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
@@ -5,10 +5,19 @@ This module provides 2 functions. send_motor_instruction and convert
 import time
 import serial
 
-# SERIAL_PORT = "/dev/cu.usbmodem1101"
-SERIAL_PORT = "/dev/ttyACM0"
+POTENTIAL_SERIAL_PORTS = ["/dev/ttyACM0", "/dev/cu.usbmodem1101"]
 
-serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None)
+serial_conn = None
+for SERIAL_PORT in POTENTIAL_SERIAL_PORTS:
+    try:
+        serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None)
+        break
+    except serial.SerialException:
+        continue
+
+if serial_conn is None:
+    raise serial.SerialException("Could not open any of the specified serial ports.")
+
 time.sleep(2)
 
 def send_motor_instruction(mode: int, direction:int, speed:int ):
