20b337c102bd0019f3a4099983f4a286bf19f788
Finn Shadow <finnandshadow@gmail.com>
2024-11-28 01:17:11 +0000

    Capped speed to not break the motors

--- nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
+++ nav_workspace/src/ros_nav/ros_nav/utils/motor_serial.py
@@ -5,19 +5,10 @@ This module provides 2 functions. send_motor_instruction and convert
 import time
 import serial
 
-POTENTIAL_SERIAL_PORTS = ["/dev/ttyACM0", "/dev/cu.usbmodem1101"]
+# SERIAL_PORT = "/dev/cu.usbmodem1101"
+SERIAL_PORT = "/dev/ttyACM0"
 
-serial_conn = None
-for SERIAL_PORT in POTENTIAL_SERIAL_PORTS:
-    try:
-        serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None)
-        break
-    except serial.SerialException:
-        continue
-
-if serial_conn is None:
-    raise serial.SerialException("Could not open any of the specified serial ports.")
-
+serial_conn = serial.Serial(port=SERIAL_PORT, baudrate=9600, timeout=None)
 time.sleep(2)
 
 def send_motor_instruction(mode: int, direction:int, speed:int ):
@@ -30,19 +21,17 @@ def send_motor_instruction(mode: int, direction:int, speed:int ):
     if (direction > 127 or direction < -128):
         raise ValueError("Invalid direction")
 
-    # We want to map [-127, -1] to [191, 255]
-    # We want to map [0, 127] to [0, 63]
-    converted_direction = None
     if (direction < 0):
-        converted_direction =  int(191 + (direction + 127) * 64 / 126)
-    else:
-        converted_direction = int(direction * 63 / 127)
+        direction = 255 - abs(direction)
+
+    direction = max(min(direction, 100), -100)
 
     output = mode.to_bytes(1, 'little', signed=True) + \
-            converted_direction.to_bytes(1, 'little', signed=False) + \
-            speed.to_bytes(1, 'little', signed=True)
+            speed.to_bytes(1, 'little', signed=True) + \
+            direction.to_bytes(1, 'little', signed=False)
     serial_conn.write(output)
 
+    read_from_arduino()
 
 def read_from_arduino():
 
